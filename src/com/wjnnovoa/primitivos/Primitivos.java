e80db1aab9811d390a5de48b32ecc186aa789f24
wjnovoam <wjnovoam@ufpso.edu.co>
2021-07-02 00:19:12 -0500

    Introduccion a los primitivos

---
+++ src/com/wjnnovoa/primitivos/Primitivos.java
@@ -0,0 +1,39 @@
+package com.wjnnovoa.primitivos;
+
+public class Primitivos {
+    //Que son los tipos primitivos
+    // -> Son tipos de datos escalares que contienen un solo valor
+    // ENTEROS -> bite, short, int, long
+    // FLOTANTE -> double, float
+    // CARACTERES -> char
+    // BOLEANOS ->
+
+    //BOOLEANOS
+
+    boolean falso = false;
+    boolean verdadero = true;
+
+    // GUARDAR UN SOLO CARACTER
+
+    char a = 'a';
+    char b  = '1';
+
+    //NUMEROS ENTEROS
+
+    //Entre -128 a 127
+    byte enteroByte = 127;
+    //Entre -32768 a 32767
+    short enteroShort = 32767;
+    //Entre -2147483648 a 2147483647
+    int enteroInt = 2147483647;
+    //Entre -9223372036854775808 a 9223372036854775807
+    long enteroLong = 9223372036854775807L;
+
+    //NUMEROS REALES
+
+    //Entre -1.4E-45 a 34.4028235E38
+    float realFloat = 3.1416f; // un float se le agrega una f a lo ultimo
+
+    //entre -4.9E-324 a 1.7976931348623157E308
+    double realDouble = 4.7029235E3;
+}
